f20ae6d532fffaf0475b3aad4460d28a92d60c04
Andrii Vasyliev <sol@hiqdev.com>
2017-05-01 12:37:48 +0200

    added BoxJson component

---
+++ src/components/BoxJson.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * Box plugin for HiDev
+ *
+ * @link      https://github.com/hiqdev/hidev-box
+ * @package   hidev-box
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hidev\box\components;
+
+use hidev\helpers\Helper;
+
+/**
+ * `box.json` config file.
+ */
+class BoxJson extends \hidev\base\ConfigFile
+{
+    protected $_file = 'box.json';
+}
